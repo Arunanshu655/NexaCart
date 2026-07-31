01861137aaaa72a37a7016e0458c3b2feff82e53
Arunanshu Dey <arunanshudey7@gmail.com>
2026-07-31 21:55:28 +0530

    "add : products route"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
 import Cart from './pages/Cart';
+import Products from './pages/Products';
 
 function App() {
   const {token} = useAuth()
@@ -36,6 +37,7 @@ function App() {
           <Route path="/login"element={<Login />}></Route>
           <Route path="/Home"element={<Home />}></Route>
           <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>}/>
+          <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>}/>
         </Routes>
       </Router>
     </>
