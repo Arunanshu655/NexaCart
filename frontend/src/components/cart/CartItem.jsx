8827354220a4856e63826d6392aa34dc83893859
Arunanshu Dey <arunanshudey7@gmail.com>
2026-08-01 22:03:52 +0530

    "feat: remove and update quantity"

--- frontend/src/components/cart/CartItem.jsx
+++ frontend/src/components/cart/CartItem.jsx
@@ -1,24 +1,104 @@
+import { useMutation } from "@apollo/client/react";
+
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+} from "../../graphql/mutations/cartMutations";
+
+import { GET_CART } from "../../graphql/queries/cartQueries";
+
 const CartItem = ({ item }) => {
 
-    return (
+  const [removeFromCart] = useMutation(REMOVE_FROM_CART, {
+    refetchQueries: [{ query: GET_CART }],
+  });
 
-        <div
-            style={{
-                border: "1px solid #ccc",
-                marginBottom: "10px",
-                padding: "15px"
-            }}
+  const [updateQuantity] = useMutation(UPDATE_CART_QUANTITY, {
+    refetchQueries: [{ query: GET_CART }],
+  });
+
+  const increaseQuantity = async () => {
+
+    await updateQuantity({
+      variables: {
+        productId: item.product.id,
+        quantity: item.quantity + 1,
+      },
+    });
+
+  };
+
+  const decreaseQuantity = async () => {
+
+    if (item.quantity === 1) {
+
+      await removeFromCart({
+        variables: {
+          productId: item.product.id,
+        },
+      });
+
+      return;
+    }
+
+    await updateQuantity({
+      variables: {
+        productId: item.product.id,
+        quantity: item.quantity - 1,
+      },
+    });
+
+  };
+
+  const removeItem = async () => {
+
+    await removeFromCart({
+      variables: {
+        productId: item.product.id,
+      },
+    });
+
+  };
+
+  return (
+
+    <div
+      style={{
+        border: "1px solid gray",
+        padding: "15px",
+        marginBottom: "10px",
+      }}
+    >
+
+      <h3>{item.product.name}</h3>
+
+      <p>₹ {item.product.price}</p>
+
+      <div>
+
+        <button onClick={decreaseQuantity}>-</button>
+
+        <span
+          style={{
+            margin: "0 10px",
+          }}
         >
+          {item.quantity}
+        </span>
 
-            <h3>{item.product.name}</h3>
+        <button onClick={increaseQuantity}>+</button>
 
-            <p>Price: ₹{item.product.price}</p>
+      </div>
 
-            <p>Quantity: {item.quantity}</p>
+      <br />
 
-        </div>
+      <button onClick={removeItem}>
+        Remove
+      </button>
 
-    );
+    </div>
+
+  );
 
 };
 
