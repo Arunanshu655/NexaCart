b336db30a0f780a1cf104815429ac1bbfad8b22c
Arunanshu Dey <arunanshudey7@gmail.com>
2026-08-02 23:35:15 +0530

    " add: review mutation"

---
+++ frontend/src/graphql/mutations/reviewMutations.js
@@ -0,0 +1,34 @@
+import { gql } from "@apollo/client/react";
+
+export const ADD_REVIEW = gql`
+mutation AddReview(
+    $productId:ID!
+    $rating:Int!
+    $comment:String!
+){
+
+    addReview(
+
+        productId:$productId
+
+        rating:$rating
+
+        comment:$comment
+
+    ){
+
+        id
+
+        rating
+
+        comment
+
+        user{
+            id
+            name
+        }
+
+    }
+
+}
+`;
