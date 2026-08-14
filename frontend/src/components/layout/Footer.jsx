dd97e2ed903501dec6a0d572f4683c5c609b7a0c
Arunanshu Dey <arunanshudey7@gmail.com>
2026-08-14 21:41:06 +0530

    "init: footer"

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+
+    return (
+
+        <footer
+            className="
+                bg-white
+                border-t
+                border-[var(--border)]
+                py-6
+                text-center
+                text-sm
+                text-[var(--muted)]
+            "
+        >
+
+            © {new Date().getFullYear()} ShopSphere.
+            All Rights Reserved.
+
+        </footer>
+
+    );
+
+};
+
+export default Footer;
