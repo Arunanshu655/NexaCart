560be949843388443e01e9fd2d3f451f8d66e742
Arunanshu Dey <arunanshudey7@gmail.com>
2026-08-22 23:55:58 +0530

    "feats: spinner"

---
+++ frontend/src/components/ui/Spinner.jsx
@@ -0,0 +1,7 @@
+const Spinner = () => {
+  return (
+    <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-200 border-t-[var(--primary)]" />
+  );
+};
+
+export default Spinner;
