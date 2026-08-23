7d5f1d624160c1e05fb3be67b52f53cb00f99225
Arunanshu Dey <arunanshudey7@gmail.com>
2026-08-23 21:47:22 +0530

    "add : wrap within wrapper"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/client/react'
 import { AuthProvider } from "./context/AuthContext";
 import client from "./apollo/client";
@@ -9,10 +10,12 @@ import client from "./apollo/client";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ApolloProvider client={client}>
-    <AuthProvider>
-    <App />
-    </AuthProvider>
-    </ApolloProvider>
+    <Router>
+      <ApolloProvider client={client}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ApolloProvider>
+    </Router>
   </StrictMode>,
 )
