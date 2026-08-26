4846c347a9e6355cd3a654679df3b11300c9d56e
Arunanshu Dey <arunanshudey7@gmail.com>
2026-08-26 21:28:44 +0530

    "add : deployed link for client"

--- frontend/src/apollo/client.jsx
+++ frontend/src/apollo/client.jsx
@@ -6,8 +6,11 @@ import {
 
 import { setContext } from "@apollo/client/link/context";
 
+const graphqlUrl =
+  import.meta.env.VITE_GRAPHQL_URL || "http://localhost:4000/graphql";
+
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: graphqlUrl,
 });
 
 const authLink = setContext((_, { headers }) => {
