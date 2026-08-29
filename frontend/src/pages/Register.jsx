a0af2995bc51859c2dd33d1b0489fb6ffb712761
Arunanshu Dey <arunanshudey7@gmail.com>
2026-08-29 13:38:11 +0530

    "add : style : register page"

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,17 +1,18 @@
-import RegisterForm from "../components/auth/RegisterFrom"
+import RegisterForm from "../components/auth/RegisterFrom";
 
 const Register = () => {
 
   return (
-
-    <div className="register-page">
-
+    <div className="
+      flex
+      min-h-[calc(100vh-160px)]
+      items-center
+      justify-center
+      py-10
+    ">
       <RegisterForm />
-
     </div>
-
   );
-
 };
 
 export default Register;
