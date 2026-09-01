c57e84d1b22f8752cf6a02d33ace0f13a8c76fe7
Arunanshu Dey <arunanshudey7@gmail.com>
2026-09-01 23:35:04 +0530

    add : gpql : chat queries"

--- frontend/src/graphql/queries/chatQueries.js
+++ frontend/src/graphql/queries/chatQueries.js
@@ -1,26 +1,49 @@
 import { gql } from "@apollo/client";
 
-export const GET_CHAT = gql`
-query GetChat($chatId: ID!) {
+export const GET_MY_CHATS = gql`
+  query MyChats {
+    myChats {
+      id
 
-  chat(chatId: $chatId) {
-
-    id
-
-    messages {
-
-      sender {
+      users {
         id
         name
+        email
       }
 
-      text
-
-      createdAt
-
+      messages {
+        id
+        text
+        sender {
+          id
+          name
+        }
+        createdAt
+      }
     }
-
   }
+`;
 
-}
+export const GET_CHAT = gql`
+  query Chat($chatId: ID!) {
+    chat(chatId: $chatId) {
+      id
+
+      users {
+        id
+        name
+        email
+      }
+
+      messages {
+        id
+        text
+        sender {
+          id
+          name
+        }
+        createdAt
+      }
+    }
+  }
 `;
