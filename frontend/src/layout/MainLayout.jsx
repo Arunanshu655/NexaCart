039c64a3c2f35e6e08a780f6ad969ed9d47714bb
Arunanshu Dey <arunanshudey7@gmail.com>
2026-09-04 20:47:33 +0530

    "modi: main layout"

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -2,29 +2,21 @@ import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 
 const MainLayout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-[var(--background)]">
 
-    return (
+      <Navbar />
 
-        <div className="min-h-screen flex flex-col bg-[var(--background)]">
-
-            <Navbar />
-
-            <main className="flex-1 pt-[72px]">
-
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-
-                    {children}
-
-                </div>
-
-            </main>
-
-            <Footer />
-
+      <main className="flex-1">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+          {children}
         </div>
+      </main>
 
-    );
+      <Footer />
 
+    </div>
+  );
 };
 
 export default MainLayout;
